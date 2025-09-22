58575669f862fa737b28faeeb6b3917ea859c45c
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-09-22 16:10:17 -0300

    prototipo de DataCleaner

--- src/helpers.py
+++ src/helpers.py
@@ -2,22 +2,12 @@ import pandas as pd
 import os
 from src.paths import DIR_DATA_PROCESADA  # carpeta de archivos procesados
 
-'''
-def leer_tsv_chunks(ruta: str):
-    return pd.read_csv(ruta, sep='\t', chunksize=10000)
-
-def guardar_csv(df, nombre_archivo,direccion_a_guardar):
-    os.makedirs(direccion_a_guardar, exist_ok=True)
-    df.to_csv(os.path.join(direccion_a_guardar, nombre_archivo), index=False, encoding="utf-8")
-    print(f"Archivo guardado: {nombre_archivo}")
-'''
-
 class DatasetManager:
   def __init__(self,base_path=None):
     """base_path: ruta base donde se guardarán o leerán los archivos.
     Si no se especifica, se usa la raíz de los datos procesados.
     """
-    self.base_path = base_path or DIR_DATA_PROCESADA     #ruta base donde se guardarán o leerán los archivos
+    self.base_path = base_path or DIR_DATA_PROCESADA     #ruta base donde se guardarán los archivos
     
   def leer_tsv_chunks(self, ruta_archivo):
     if not os.path.exists(ruta_archivo):
@@ -31,3 +21,27 @@ class DatasetManager:
     df.to_csv(ruta, index=False, encoding="utf-8")
     #print(f"Archivo guardado en: {ruta}")
     print(f"Archivo guardado: {nombre_archivo}")
+
+
+
+class DataCleaner(DatasetManager):
+  def __init__(self, ruta_archivo, dir_salida=None):
+    super().__init__(dir_salida)
+    self.ruta_archivo = ruta_archivo  # Archivo de entrada específico
+
+  def mostrarContenidoDeUnaColumna(self, nombre_columna):  
+    listado_iterable=self.leer_tsv_chunks(self.ruta_archivo)
+
+    listado= set()
+
+    # Leer chunk por chunk y añadir los tipos únicos al conjunto
+    for chunk in listado_iterable:
+      listado.update(chunk[nombre_columna].unique())
+    
+    # Convertir a lista y ordenar alfabéticamente
+    return sorted(list(listado))
+  
+  def limpiar():
+    pass
+
+
