b74b1900fec50aefae43df0580c733490d3190e1
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-09-30 14:39:19 -0300

    Refactorizo el script de procesamiento de nombres y actualizo titles notebook

--- src/data_analysis/name.py
+++ src/data_analysis/name.py
@@ -1,23 +1,33 @@
+"""
+Correr con:
+    python -m src.data_analysis.name
+"""
+import os
+import sys
 import pandas as pd
-from ..paths import RUTA_NOMBRE
-from ..helpers import leer_tsv_chunks
 
-# Conjunto para almacenar profesiones únicas
-profesiones_unicas = set()
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
 
-# Leer chunk por chunk
-nombres_iterable = leer_tsv_chunks(RUTA_NOMBRE)
-for chunk in nombres_iterable:
-    # Filtrar filas donde primaryProfession no sea nulo o '\N'
-    profesiones_validas = chunk[~chunk['primaryProfession'].isna() & (chunk['primaryProfession'] != '\\N')]
-    
-    # Para cada fila, dividir las profesiones y añadirlas al conjunto
-    for profesiones in profesiones_validas['primaryProfession']:
-        profesiones_unicas.update(profesiones.split(','))
+from src.paths import RUTA_NOMBRE
+from src.paths import RUTA_PRINCIPALES_2019
+from src.paths import DIR_DATA_PROCESADA
 
-# Convertir a lista y ordenar alfabéticamente
-lista_profesiones = sorted(list(profesiones_unicas))
-print(f"Total de profesiones únicas encontradas: {len(lista_profesiones)}")
-print("\nLista de profesiones:")
-for profesion in lista_profesiones:
-    print(f"- {profesion}")
+from src.helpers import DataCleaner
+
+def main():
+  df_principals = pd.read_csv(RUTA_PRINCIPALES_2019)  #de aca saco los "nconst"
+
+  cleaner = DataCleaner(RUTA_NOMBRE , DIR_DATA_PROCESADA)
+
+  columnas_a_mantener = ['nconst', 'primaryName', 'birthYear','primaryProfession']
+
+  nconsts_relevantes = set(df_principals["nconst"].astype(str))
+
+
+  df_names = cleaner.limpiar(filtros={'nconst': list(nconsts_relevantes)})
+  df_names = cleaner.mantener_columnas(df_names,columnas_a_mantener)
+
+
+  cleaner.guardar_csv(df_names, "nombres_2019.csv")
+if __name__ == "__main__":
+  main()
