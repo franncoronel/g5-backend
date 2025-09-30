6a1e6d92b16643f59ec562fcba11da5b6d0ff452
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-09-30 14:40:00 -0300

    Agrego rutas para archivos filtrados de 2019 en paths.py

--- src/paths.py
+++ src/paths.py
@@ -18,3 +18,8 @@ RUTA_ALIAS= os.path.join(DIR_DATA,"title.akas.tsv")
 
 # Ruta de archivos filtrados
 RUTA_TITULO_2019 = os.path.join(DIR_DATA_PROCESADA,"peliculas_2019.csv")
+RUTA_ALIAS_2019 = os.path.join(DIR_DATA_PROCESADA,"alias_2019.csv")
+RUTA_CRITICAS_2019 = os.path.join(DIR_DATA_PROCESADA,"criticas_2019.csv")
+RUTA_PERSONAL_2019 = os.path.join(DIR_DATA_PROCESADA,"crew_2019.csv")
+RUTA_PRINCIPALES_2019 =os.path.join(DIR_DATA_PROCESADA,"principales_2019.csv")
+RUTA_NOMBRE_2019 = os.path.join(DIR_DATA_PROCESADA, "nombres_2019.csv")
