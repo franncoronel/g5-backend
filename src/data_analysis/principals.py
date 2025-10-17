0c145727a8f1e7d57ede513334b515bbed765f47
Joaquín Navarro <joanavarro23@hotmail.com>
2025-10-16 22:49:35 -0300

    corrijo bug lista principals

--- src/data_analysis/principals.py
+++ src/data_analysis/principals.py
@@ -29,8 +29,7 @@ def main():
 
   df_principals = cleaner.mantener_columnas(df_principals, columnas_a_mantener)
 
-  df_principals['category'] = df_principals['category'].apply(
-                                  lambda lista: ['actor' if p.lower() == 'actress' else p for p in lista])
+  df_principals['category'] = df_principals['category'].replace('actress', 'actor')
 
   cleaner.guardar_csv(df_principals, "principales_2019.csv")
 
