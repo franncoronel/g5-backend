f12f8916b7bf2c15ee3123354149a1e582cce3d9
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-10-17 11:04:19 -0300

    corrijo bug

--- src/data_analysis/name.py
+++ src/data_analysis/name.py
@@ -23,15 +23,9 @@ def main():
 
   nconsts_relevantes = set(df_principals["nconst"].astype(str))
 
-
   df_names = cleaner.limpiar(filtros={'nconst': list(nconsts_relevantes)})
   
   df_names = cleaner.mantener_columnas(df_names,columnas_a_mantener)
-
-  df_names = cleaner.columna_como_lista(df_names,'primaryProfession')
-
-  df_names['primaryProfession'] = df_names['primaryProfession'].apply(
-                                  lambda lista: ['actor' if p.lower() == 'actress' else p for p in lista])
   
   cleaner.guardar_csv(df_names, "nombres_2019.csv")
 if __name__ == "__main__":
