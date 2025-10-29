fc6234e9feaca87f56a56c6c80f822065e76d9eb
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-10-29 01:30:24 -0300

    Refactor script de carga de datos para mejorar la ejecución de comandos

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,26 +1,34 @@
-import pandas as pd
-from src.database.entidades import motor
-from src.paths import RUTA_TITULO_2019, RUTA_ALIAS_2019, RUTA_CRITICAS_2019, RUTA_NOMBRE_2019, RUTA_PRINCIPALES_2019
+'''
+Correr con:
+          python scripts/load_data.py
+'''
 
-# 2. Carga tu CSV limpio a un DataFrame de Pandas
-df_titulos = pd.read_csv(RUTA_TITULO_2019)
+import subprocess
+import sys
 
-df_titulos = df_titulos.drop('originalTitle', 'genres')
+def correr_comando(comando):
+  print(f"\n>>> Ejecutando: {comando}")
+  result = subprocess.run(comando, shell=True)
+  if result.returncode != 0:
+    print(f"❌ Error al ejecutar: {comando}")
+    sys.exit(result.returncode)
+  
+  print(f"✅ Finalizado: {comando}")
 
-# ¡IMPORTANTE!
-# Asegúrate de que los nombres de las columnas en tu CSV coincidan
-# EXACTAMENTE con los nombres de las columnas en tu tabla de la base de datos.
-# Si no coinciden, puedes renombrarlos en Pandas:
-# df = df.rename(columns={'titulo_csv': 'nombre_columna_db'})
+def main():
 
-df_titulos = df_titulos.rename(columns={'tconst': 'id','titleType': 'tipo','primaryTitle':'titulo',})
+  comandos = ['python -m src.database.entidades',
+               'python -m scripts.data_loaders.title',
+               'python -m scripts.data_loaders.names',
+               'python -m scripts.data_loaders.genres',
+               'python -m scripts.data_loaders.title_genre',
+               'python -m scripts.data_loaders.ratings'
+               ]
+  
+  for cmd in comandos:
+    correr_comando(cmd)
+  
+  print("\nTodos los scripts se ejecutaron correctamente.")
 
-# 3. Carga el DataFrame a la tabla
-print("Cargando datos a la tabla 'titulos'...")
-df_titulos.to_sql(
-    'titulo',          # Nombre de la tabla en la BD
-    con=motor,        # El motor de SQLAlchemy
-    if_exists='append', # 'append' = añade los datos, 'replace' = borra y crea, 'fail' = falla si existe
-    index=False        # No guardes el índice de Pandas como una columna
-)
-print("¡Carga completada!")
+if __name__ == "__main__":
+  main()
