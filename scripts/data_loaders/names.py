2e0b627561c840c9b09783408d484985b67cab1e
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-10-28 23:36:07 -0300

    Refactorizo la carga de datos de nombres para utilizar DBLoader

--- scripts/data_loaders/names.py
+++ scripts/data_loaders/names.py
@@ -1,26 +1,21 @@
 # Ejecutar con "python -m scripts.data_loaders.names"
 
-import pandas as pd
 from src.database.entidades import motor
+from src.helpers import DBLoader
 from src.paths import RUTA_NOMBRE_2019
-
-# 2. Carga tu CSV limpio a un DataFrame de Pandas
-df_nombres = pd.read_csv(RUTA_NOMBRE_2019)
 
 # ¡IMPORTANTE!
 # Asegúrate de que los nombres de las columnas en tu CSV coincidan
 # EXACTAMENTE con los nombres de las columnas en tu tabla de la base de datos.
-# Si no coinciden, puedes renombrarlos en Pandas:
-# df = df.rename(columns={'titulo_csv': 'nombre_columna_db'})
+# Si no coinciden,  hay renombrarlos, arma un diccionario
+#{'titulo_csv': 'nombre_columna_db'}
 
-df_nombres = df_nombres.rename(columns={'nconst': 'id','primaryName': 'nombre'})
 
-# 3. Carga el DataFrame a la tabla
-print("Cargando datos a la tabla 'persona'...")
-df_nombres.to_sql(
-    'persona',          # Nombre de la tabla en la BD
-    con=motor,        # El motor de SQLAlchemy
-    if_exists='replace', # 'append' = añade los datos, 'replace' = borra y crea, 'fail' = falla si existe
-    index=False        # No guardes el índice de Pandas como una columna
+loader = DBLoader(motor)    # El motor de SQLAlchemy
+
+loader.cargar_csv(
+    ruta_csv=RUTA_NOMBRE_2019,
+    tabla='persona', # Nombre de la tabla en la BD
+    renombrar={'nconst': 'id','primaryName': 'nombre'},
+    modo='replace'  # 'append' = añade los datos, 'replace' = borra y crea, 'fail' = falla si existe
 )
-print("¡Carga completada!")
