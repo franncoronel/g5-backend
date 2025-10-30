0e60903863b51b52e22040a0b6a0e3c3e215e6f6
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-10-30 16:40:41 -0300

    cargo los titulos alternativos en la base de datos

---
+++ scripts/data_loaders/akas.py
@@ -0,0 +1,17 @@
+# Ejecutar con "python -m scripts.data_loaders.akas"
+
+from src.database.entidades import motor
+from src.helpers import DBLoader
+from src.paths import RUTA_ALIAS_2019
+
+
+loader = DBLoader(motor)    # El motor de SQLAlchemy
+
+loader.cargar_csv(
+  ruta_csv=RUTA_ALIAS_2019,
+  tabla="titulo_alternativo", # Nombre de la tabla en la BD
+  renombrar={"tconst": "id_titulo","title": "titulo",
+             "isOriginalTitle": "es_original", "language":"idioma"},
+  modo="append",  # 'append' = añade los datos, 'replace' = borra y crea, 'fail' = falla si existe
+  auto_id=True
+)
