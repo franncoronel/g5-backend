7907cbe8ffad2bd179bb33a00961c03432302e51
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-10-29 23:32:31 -0300

    cargo el archivo de profesiones a la base de datos

---
+++ scripts/data_loaders/professions.py
@@ -0,0 +1,16 @@
+
+# Ejecutar con "python -m scripts.data_loaders.professions"
+
+from src.database.entidades import motor
+from src.helpers import DBLoader
+from src.paths import RUTA_PROFESIONES
+
+
+loader = DBLoader(motor)    # El motor de SQLAlchemy
+
+loader.cargar_csv(
+  ruta_csv=RUTA_PROFESIONES,
+  tabla="profesion", # Nombre de la tabla en la BD
+  renombrar={"id_profesion": "id","typeProfession": "nombre"},
+  modo="append"  # 'append' = añade los datos, 'replace' = borra y crea, 'fail' = falla si existe
+)
