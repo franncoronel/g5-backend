230d0efe8015543992cd027ca4eeb8ec80df2644
juaanicl <juanicluna@gmail.com>
2025-11-03 20:21:11 -0300

    agrego permisos al front para conectarse

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 DB_PATH = "data/recomendador.sqlite"  
 @app.get("/generos")
