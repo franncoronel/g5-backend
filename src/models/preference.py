502e2261a4eae676f8de21e309a7a6646ac726f4
juaanicl <juanicluna@gmail.com>
2025-11-06 19:37:57 -0300

    creo objeto preferencia

--- src/models/preference.py
+++ src/models/preference.py
@@ -1,9 +1,21 @@
 from pydantic import BaseModel
 from typing import List, Optional, Tuple
 
+from src.database.entidades import Genero, Persona
+
+
 class PreferenceDTO(BaseModel):
     genres: Optional[List[int]] = None
     yearRange: Tuple[int, int]
     duration: Tuple[int, int]
-    actors: Optional[List[int]] = None
-    directors: Optional[List[int]] = None
+    actors: Optional[List[str]] = None
+    directors: Optional[List[str]] = None
+
+#    def createPreference():
+
+class Preference():
+    genres: Optional[List[Genero]]
+    #yearRange: Tuple[]
+    duration: Tuple[int,int]
+    actors: Optional[List[Persona]]
+    directors: Optional[List[Persona]]
