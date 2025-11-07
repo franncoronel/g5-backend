552ec4628d248cc46f1372337d9e86af71d5f13f
juaanicl <juanicluna@gmail.com>
2025-11-07 11:15:41 -0300

    corrijo mapeo generos

--- src/models/preference.py
+++ src/models/preference.py
@@ -24,34 +24,58 @@ class Preference():
     directors: Optional[List[Persona]]
 
 def mapPreference(preference:PreferenceDTO):
-    conn, cursor=conectarBase
+    conn, cursor=conectarBase()
     try:
         genres = mapGenres(cursor,preference.genres)
+        actors = mapActors(cursor,preference.actors)
+        directors=mapDirectors(cursor,preference.directors)
+        
+        mappedPreference = Preference(
+            genres=genres,
+            duration=preference.duration,
+            actors=actors,
+            directors=directors
+    )
 
     finally:
         conn.close()
     
-
+    return mappedPreference
 
 def conectarBase():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     return conn, cursor
 
-def mapGenres(cursor:sqlite3.Cursor,genresID:List[int]):
+def mapGenres(cursor: sqlite3.Cursor, genresID: list[int]):
     genres = []
-    for genreID in genresID:
-        cursor.execute("SELECT * FROM genero",(genreID))
-        genero=cursor.fetchone()
-        if genero:
-            genres.append(genero)
-    
+    for genre_id in genresID:
+        cursor.execute("SELECT id, nombre FROM genero WHERE id = ?", (genre_id,))
+        row = cursor.fetchone()
+        if row:
+            genres.append(Genero(id=row[0], nombre=row[1]))
     return genres
     
 
-# def mapActors(cursor:sqlite3.cursor,actorsName:List[str]):
-#     actors = []
-#     for actorName in actorsName:
-#         cursor.execute("SELECT * FROM profesion_titulo")
+def mapActors(cursor: sqlite3.Cursor, actorsName: list[str]):
+    actors = []
+    for actorName in actorsName:
+        cursor.execute("""
+            SELECT * FROM persona
+            WHERE LOWER(nombre) LIKE ?
+        """, (f"%{actorName.lower()}%",))
+        coincidencias = cursor.fetchall()
+        actors.extend(coincidencias)
+    return actors
     
     
+def mapDirectors(cursor: sqlite3.Cursor,directorsName: list[str]):
+    directors=[]
+    for directorName in directorsName:
+        cursor.execute("""
+            SELECT * FROM persona
+            WHERE LOWER(nombre) LIKE ?
+        """, (f"%{directorName.lower()}%",))
+        coincidencias = cursor.fetchall()
+        directors.extend(coincidencias)
+    return directors
